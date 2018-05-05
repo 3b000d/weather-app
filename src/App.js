ae84f391babd2f3aee7c101a7115c0cd4897f169
abdelhadi mohammed <xxxx7028@gmail.com>
2018-05-05 16:45:48 +0200

    it was not working cuz it was not https and i update it :)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         var city = e.target.elements.city.value;
         var country = e.target.elements.country.value;
 
-        const api_call =await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&forecast?id=524901&APPID=5b29699cde634966d45662a601d47cdc");
+        const api_call =await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&forecast?id=524901&APPID=5b29699cde634966d45662a601d47cdc");
         const data = await api_call.json();
 
         if (city && country){
